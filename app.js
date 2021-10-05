24c0b596b3d0fdea5447c1acb9aeb385b4792ac9
ClarkUPS <90676910+ClarkUPS@users.noreply.github.com>
2021-10-04 17:38:51 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,30 +1,28 @@
 
-
-
 const wordLength = 6;
-var validWords = {}; //make Hashmap
+let validWords = new Map(); //make Hashmap
 var validSeedWords = [];
+var sucsessfulWordScramble = [];
 var mainWord;
 
 
 
 
-
+//refine words by length
+//Populate Hashmap
 for (let i = 0; i < dictionary.length; i++){
-    if (dictionary[i].length > 3 && dictionary[i].length <= 6){
-        validWords[dictionary[i]] = true; //= to asign key and value
+    if (dictionary[i].length >= 3 && dictionary[i].length <= 6){
+        validWords.set(dictionary[i]); //= to asign key and value
         if(dictionary[i].length == 6){
             validSeedWords.push(dictionary[i]);
         }
     }
 }
-
+//selecty random word
 let randWord = Math.floor(Math.random()*validSeedWords.length);
 mainWord = validSeedWords[randWord];
 
-//Populate Hashmap
 console.log(mainWord);
-console.log
 
 //list of letters 
 var letterList = [];
@@ -32,10 +30,9 @@ for(let i = 0; i < wordLength; i ++){
 letterList.push((mainWord).substring(i ,i+1));
 }
 
-var letterBase = letterList
-letterList.forEach(function(entry){
-    console.log(entry);
-});
+for
+
+
 
 
 
@@ -52,43 +49,44 @@ letterList.forEach(function(entry){
 //}
 
 
-var a =1;
-var b =1;
-var c =1;
-var d =1;
-var e =1;
-var f =1;
-var runningTotal = 0;
-while (f < 6 ){
-    console.log(a + b +c + d+ e+ f)
-    runningTotal++
-    console.log("running total " + runningTotal);
-    console.log("End condition " + this.f);
-    a++
-    if (a > 6){
-        b++
-        a=0;
-        if(b > 6){
-            c++
-            b = 0;
-            if (c > 6){
-                d++
-                c = 0;
-                if(d > 6){
-                    e++;
-                    d = 0;
-                    if(e > 6){
-                        f++
-                        e = 0;
-                    }
-                }
-            }
-        }
-    }
+var a =0;
+var b =0;
+var c =0;
+var d =0;
+var e =0;
+var f =0;
+const layer1 = 120;
+const layer2 = 24
+const layer3 = 6
+const layer4 = 2
+const layer5 = 1
 
+
+
+
+console.log(letterList);
+
+
+function checkHashmap(){
+if(runningTotal < 216){
+    
+    threeLetterWord = (letterList[a]+letterList[b]+letterList[c]);
+    console.log(threeLetterWord)
+    console.log(validWords.has(threeLetterWord));
+    if(validWords.has(threeLetterWord)){
+        validWords.push(threeLetterWord);
+        console.log("sucsess!")
+    }
+        //sucsessfulWordScramble
+    
+}
 
 }
-    
+
+console.log("length "+validWords.size)
+
+   
+console.log(validWords.has("abase"));
 
  
  
